1392f045cf2e5756b729a23e3bb76de61aeeedf3
shokhruzbek <ashokhruzbek@gmail.com>
2025-07-15 00:26:16 +0500

    add getData function to provide a list of programming languages with pricing and images

---
+++ src/constants/db.js
@@ -0,0 +1,64 @@
+export function getData() {
+  return [
+    {
+      title: 'JavaScript',
+      price: 17.99,
+      Image: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
+      id: 1,
+    },
+    {
+      title: 'Python',
+      price: 19.99,
+      Image: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg',
+      id: 2,
+    },
+    {
+      title: 'Java',
+      price: 21.5,
+      Image: 'https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg',
+      id: 3,
+    },
+    {
+      title: 'C++',
+      price: 18.0,
+      Image: 'https://upload.wikimedia.org/wikipedia/commons/1/18/ISO_C%2B%2B_Logo.svg',
+      id: 4,
+    },
+    {
+      title: 'React',
+      price: 22.99,
+      Image: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+      id: 5,
+    },
+    {
+      title: 'Node.js',
+      price: 20.5,
+      Image: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg',
+      id: 6,
+    },
+    {
+      title: 'HTML5',
+      price: 15.0,
+      Image: 'https://upload.wikimedia.org/wikipedia/commons/3/38/HTML5_Badge.svg',
+      id: 7,
+    },
+    {
+      title: 'CSS3',
+      price: 14.0,
+      Image: 'https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg',
+      id: 8,
+    },
+    {
+      title: 'TypeScript',
+      price: 19.0,
+      Image: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg',
+      id: 9,
+    },
+    {
+      title: 'Docker',
+      price: 23.5,
+      Image: 'https://upload.wikimedia.org/wikipedia/commons/4/4e/Docker_%28container_engine%29_logo.svg',
+      id: 10,
+    },
+  ];
+}
